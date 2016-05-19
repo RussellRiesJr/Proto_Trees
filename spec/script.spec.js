7a15ff7272576c11c10d5f6e704aba02753cea64
Russell Ries <riesru@gmail.com>
2016-05-19 11:53:12 -0500

    add basic set up

---
+++ spec/script.spec.js
@@ -0,0 +1,12 @@
+"use strict";
+
+describe ("Plant Function", Function() {
+
+  var testPlant = new Plant;
+
+  it("should exist", Function() {
+    expect(testPlant).toBeDefined();
+  });
+
+  it("")
+});
